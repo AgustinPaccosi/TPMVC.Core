142bf8f3ab10ae8ea2bec41ac93c83ec37e1caa9
AgustinPaccosi <Aguspaccosi@hotmail.com>
2025-03-04 17:49:02 -0300

    ActFinal 4-3-24

--- TPMVC.Core.Web/wwwroot/js/order.js
+++ TPMVC.Core.Web/wwwroot/js/order.js
@@ -6,7 +6,7 @@ $(function () {
 function loadDataTable() { //En esta función le indico que se llene la tabla
     dataTable = new DataTable('#orderTable', { //Este es el id que le di en el index  y el # es como se inicializa la tabla que traigo de la pág DataTable
         "ajax": {
-            "url": "/Customer/Orders/GetAll" //En esta url me traigo los datos
+            "url": "/Customer/Order/GetAll" //En esta url me traigo los datos
         },
         "columns": [ //Creo las columnas y como son varias las pongo dentro de un Array
             { "data": "orderHeaderId" },//Se utiliza la minuscula en el nombre de la columna ya que así es como es la convención en JavaScript                         //y porque así viene serializado en Json
@@ -32,7 +32,7 @@ function loadDataTable() { //En esta función le indico que se llene la tabla
                 "data": "orderHeaderId",
                 "render": function (data) {                    //Le indico la ruta por hacía donde tiene que ir con un href ya que el asp acá no funciona debido a que es JavaScript y a través del data le paso el id del OrderHeader
                     return `
-                             <a class="btn btn-info" href="/Customer/Orders/Details?id=${data}"> 
+                             <a class="btn btn-info" href="/Customer/Order/Details?id=${data}"> 
                                 <i class="bi bi-card-list"></i>&nbsp;
                                 Details
                             </a>
